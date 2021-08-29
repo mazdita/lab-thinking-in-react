98c853408c0e7f73835a30c32aa03d91f0aa411d
mazdita <mazdita@hotmail.com>
2021-08-29 18:31:55 +0200

    thinking

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
+import React from 'react';
+import jsondata from './data/data.json';
+import './App.css';
+import FilterableProductTable from './components/FilterableProductTable';
+import { render } from '@testing-library/react';
+
+
+class App extends React.Component {
+  render() {
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FilterableProductTable products={ jsondata.data } />
     </div>
-  );
+    )
+  };
 }
 
 export default App;
---
+++ src/components/FilterableProductTable.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class FilterableProductTable extends React.Component{
+
+    render(){
+        return(
+        <h1>IronStore</h1>
+        )
+    }
+}
+
+export default FilterableProductTable
